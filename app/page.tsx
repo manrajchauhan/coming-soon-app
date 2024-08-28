8aba8c716af7648977c6bb156651dce8502f9666
manrajchauhan <m900413089@gmail.com>
2024-08-28 19:31:18 +0530

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,131 @@
+"use client"
 import Image from "next/image";
+import { useAnimate } from "framer-motion";
+import { useEffect, useRef, useState } from "react";
 
-export default function Home() {
+const COUNTDOWN_FROM = "2024-09-01";
+
+const SECOND = 1000;
+const MINUTE = SECOND * 60;
+const HOUR = MINUTE * 60;
+const DAY = HOUR * 24;
+
+const ShiftingCountdown = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <main className="flex flex-col items-center justify-between p-24 ">
+    <div className="z-10 w-full max-w-6xl items-center justify-between font-mono text-sm lg:flex">
+      <Image
+        src="/brand.svg"
+        alt="brand Logo"
+        width={200}
+        height={24}
+        priority
+      />
+        <h1>Equiflex Private Limited </h1>
+    </div>
+    <div className="mt-20">
+      <div className="mx-auto flex w-full max-w-5xl items-center gap-20 text-center ">
+        <CountdownItem unit="Day" text="days" />
+        <CountdownItem unit="Hour" text="hours" />
+        <CountdownItem unit="Minute" text="minutes" />
+        <CountdownItem unit="Second" text="seconds" />
+      </div>
+    </div>
+    <div className="mt-20">
+        <h1 className="text-2xl font-semibold text-[#22b1ad] text-center">
+        Something Awesome Is on Its Way!
+          <br />
+          <span className="text-4xl md:text-[6rem] font-bold mt-1 text-center leading-none text-black">
+          COMING SOON
+          </span>
+        </h1>
       </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
   );
+};
+
+interface CountdownItemProps {
+  unit: "Day" | "Hour" | "Minute" | "Second";
+  text: string;
 }
+
+const CountdownItem: React.FC<CountdownItemProps> = ({ unit, text }) => {
+  const { ref, time } = useTimer(unit);
+
+  return (
+
+      <div className="flex h-24 w-1/4 flex-col items-center justify-center gap-1 border-r-[1px] border-slate-200 font-mono md:h-36 md:gap-2 p-4">
+        <div className="relative w-full overflow-hidden text-center">
+          <span
+            ref={ref}
+            className="block text-2xl font-medium text-[#22b1ad] md:text-4xl lg:text-6xl xl:text-7xl"
+          >
+            {time}
+          </span>
+        </div>
+        <span className="text-xs font-light text-neutral-500 md:text-sm lg:text-base">
+          {text}
+        </span>
+      </div>
+  );
+};
+
+export default ShiftingCountdown;
+
+const useTimer = (unit: "Day" | "Hour" | "Minute" | "Second") => {
+  const [ref, animate] = useAnimate();
+  const intervalRef = useRef<NodeJS.Timeout | null>(null);
+  const timeRef = useRef<number>(0);
+
+  const [time, setTime] = useState<number>(0);
+
+  useEffect(() => {
+    intervalRef.current = setInterval(handleCountdown, 1000);
+
+    return () => {
+      if (intervalRef.current) {
+        clearInterval(intervalRef.current);
+      }
+    };
+  }, []);
+
+  const handleCountdown = async () => {
+    const end = new Date(COUNTDOWN_FROM);
+    const now = new Date();
+    const distance = +end - +now;
+
+    let newTime = 0;
+
+    if (unit === "Day") {
+      newTime = Math.floor(distance / DAY);
+    } else if (unit === "Hour") {
+      newTime = Math.floor((distance % DAY) / HOUR);
+    } else if (unit === "Minute") {
+      newTime = Math.floor((distance % HOUR) / MINUTE);
+    } else {
+      newTime = Math.floor((distance % MINUTE) / SECOND);
+    }
+
+    if (newTime !== timeRef.current) {
+      // Exit animation
+      await animate(
+        ref.current,
+        { y: ["0%", "-50%"], opacity: [1, 0] },
+        { duration: 0.35 }
+      );
+
+      timeRef.current = newTime;
+      setTime(newTime);
+
+      // Enter animation
+      await animate(
+        ref.current,
+        { y: ["50%", "0%"], opacity: [0, 1] },
+        { duration: 0.35 }
+      );
+    }
+  };
+
+  return { ref, time };
+};
